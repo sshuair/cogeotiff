b986658a932ed93f9444acd8a784f313352c5af5
sshuair <sshuair@gmail.com>
2019-03-25 17:32:30 +0800

    fix docs

--- cogeotiff/cli/create.py
+++ cogeotiff/cli/create.py
@@ -6,7 +6,7 @@ from cogeotiff.cog import create_cog
 @click.argument('dst_path')
 @click.option('--overview-resampling', 
              default='nearest',
-             help='resampling method for create pyramiddefault: nearest'
+             help='resampling method for create pyramid, default: nearest'
              )
 @click.option('--overview-level', 
              type=int, 
@@ -20,13 +20,13 @@ from cogeotiff.cog import create_cog
              )
 @click.option('--block-size', 
              type=int, 
-             default=512,
+             default=256,
              help='tiled size, default: 512'
              )
 @click.option('--compress', 
              type=str, 
              default='raw',
-             help='compress method,default: raw')
+             help='compress method: JPEG,RAW,DEFLATE,LZW,WEBP,ZSTD,PACKBITS,default: raw')
 def create_cog_command(
     src_path,
     dst_path, 
